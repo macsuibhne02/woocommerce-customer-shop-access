e65f9cb79273aadcd1782dc81288c0162d5bec39
macsuibhne02 <mac_suibhne@yahoo.com>
2026-07-17 12:52:02 +0100

    Implement boot and init methods in Plugin class

--- woocommerce-customer-shop-access/includes/Core/Plugin.php
+++ woocommerce-customer-shop-access/includes/Core/Plugin.php
@@ -1,1 +1,22 @@
+<?php
 
+declare(strict_types=1);
+
+namespace WCCSA\Core;
+
+final class Plugin
+{
+    public function boot(): void
+    {
+        add_action('plugins_loaded', [$this, 'init']);
+    }
+
+    public function init(): void
+    {
+        load_plugin_textdomain(
+            Constants::text_domain(),
+            false,
+            dirname(plugin_basename(Constants::plugin_file())) . '/languages'
+        );
+    }
+}
