f18a8283bce26aca0e00ec1f7d607e833373471d
macsuibhne02 <mac_suibhne@yahoo.com>
2026-07-17 12:51:48 +0100

    Add Constants class for plugin configuration

--- woocommerce-customer-shop-access/includes/Core/Constants.php
+++ woocommerce-customer-shop-access/includes/Core/Constants.php
@@ -1,1 +1,33 @@
+<?php
 
+declare(strict_types=1);
+
+namespace WCCSA\Core;
+
+final class Constants
+{
+    public static function version(): string
+    {
+        return '0.2.0';
+    }
+
+    public static function text_domain(): string
+    {
+        return 'woocommerce-customer-shop-access';
+    }
+
+    public static function plugin_file(): string
+    {
+        return dirname(__DIR__, 2) . '/woocommerce-customer-shop-access.php';
+    }
+
+    public static function plugin_path(): string
+    {
+        return dirname(__DIR__, 2) . '/';
+    }
+
+    public static function plugin_url(): string
+    {
+        return plugin_dir_url(self::plugin_file());
+    }
+}
